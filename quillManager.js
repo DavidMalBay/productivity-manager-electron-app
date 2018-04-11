eceb2f343876ba594f3d8b92916ee07053bd135e
David Maldonado Garibay <david.maldonado41g@gmail.com>
2018-04-10 21:52:42 -0700

    fixed toolbar order

--- quillManager.js
+++ quillManager.js
@@ -2,6 +2,31 @@ const Quill = require('quill');
 
 
 var container = $('#quillEditor').get(0)
+var toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+  ['blockquote', 'code-block'],
+  [{
+    'header': [1, 2, 3, 4, 5, 6, false]
+  }],
+  ['link', 'image'], // add's image support
+  [{
+    'list': 'ordered'
+  }, {
+    'list': 'bullet'
+  }], // outdent/indent
+  [{
+    'color': []
+  }, {
+    'background': []
+  }], // dropdown with defaults from theme
+  [{
+    'font': []
+  }],
+  [{
+    'align': []
+  }],
+  ['clean'] // remove formatting button
+];
 var quill = new Quill(container, {
   modules: {
     toolbar: toolbarOptions,
@@ -10,31 +35,7 @@ var quill = new Quill(container, {
     theme: 'snow',
   });
     
-var toolbarOptions = [
-    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-    ['blockquote', 'code-block'],
-    [{
-      'header': [1, 2, 3, 4, 5, 6, false]
-    }],
-    ['link', 'image'], // add's image support
-    [{
-      'list': 'ordered'
-    }, {
-      'list': 'bullet'
-    }], // outdent/indent
-    [{
-      'color': []
-    }, {
-      'background': []
-    }], // dropdown with defaults from theme
-    [{
-      'font': []
-    }],
-    [{
-      'align': []
-    }],
-    ['clean'] // remove formatting button
-  ];
+
   
     $("#note-title").keydown(function (e) {    
       if (e.which == 9) {
